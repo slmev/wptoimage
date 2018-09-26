92c69f65e35ff2b29b9f622a05fb44a9e53f7d8b
neo <zhujie1993@hotmail.com>
2018-09-26 16:31:47 +0800

    页面截图简易实现

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
 
 const program = require("commander");
+const puppeteer = require('puppeteer');
+const path = require('path');
 const appInfo = require("./package.json");
 
-program.allowUnknownOption();
-program.version(appInfo.version);
+program.allowUnknownOption()
+       .version(appInfo.version, '-v, --version')
+       .usage('[options] <input file> <output file>')
+    //    .option('--shot-w <int>', '设置图片宽度', (e) => { console.log(e); return e; })
+    //    .option('--shot-h <int>', '设置图片高度', (e) => { console.log(e); return e; })
+       .action(function(inputFile, outputFile) {
+           (async () => {
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                await page.goto(path.resolve(inputFile));
+                await page.setViewport({
+                    width: 790,
+                    height: 1,
+                });
+                await page.screenshot({
+                path: path.resolve(outputFile),
+                    type: 'jpeg',
+                    fullPage: true,
+                });
+                await browser.close();
+            })();
+       })
+       .parse(process.argv);
 
-program.parse(process.argv);

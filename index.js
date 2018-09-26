fcb4be69b4bfe88f8627f23a66d64b3a84dadb25
neo <zhujie1993@hotmail.com>
2018-09-26 18:39:38 +0800

    部分属性配置实现

--- index.js
+++ index.js
@@ -8,24 +8,25 @@ const appInfo = require("./package.json");
 program.allowUnknownOption()
        .version(appInfo.version, '-v, --version')
        .usage('[options] <input file> <output file>')
-    //    .option('--shot-w <int>', '设置图片宽度', (e) => { console.log(e); return e; })
-    //    .option('--shot-h <int>', '设置图片高度', (e) => { console.log(e); return e; })
+       .option('--shot-w <int>', '设置图片宽度')
+       .option('--shot-h <int>', '设置图片高度')
+       .option('--full-page <bool>', '设置是否截取完整页面, 默认为true')
        .action(function(inputFile, outputFile) {
+            const { shotW, shotH, fullPage } = program;
            (async () => {
                 const browser = await puppeteer.launch();
                 const page = await browser.newPage();
                 await page.goto(path.resolve(inputFile));
                 await page.setViewport({
-                    width: 790,
-                    height: 1,
+                    width: Number(shotW) || 790,
+                    height: Number(shotH) || (fullPage === 'false' ? 600 : 1),
                 });
                 await page.screenshot({
                 path: path.resolve(outputFile),
                     type: 'jpeg',
-                    fullPage: true,
+                    fullPage: fullPage === 'false' ? false : true,
                 });
                 await browser.close();
             })();
        })
        .parse(process.argv);
-

ff672092536cb41f51315ca93cb479c0dafe713f
neo <zhujie1993@hotmail.com>
2018-09-28 18:45:51 +0800

    添加系统判断修复linux本地文件无法识别的问题以及添加通过网址生成图片的功能

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const program = require("commander");
 const puppeteer = require('puppeteer');
 const path = require('path');
+const os = require('os');
+const fs = require('fs');
 const appInfo = require("./package.json");
 
 program.allowUnknownOption()
@@ -13,10 +15,39 @@ program.allowUnknownOption()
        .option('--full-page <bool>', '设置是否截取完整页面, 默认为true')
        .action(function(inputFile, outputFile) {
             const { shotW, shotH, fullPage } = program;
+            console.log('----------');
+            console.log(inputFile, outputFile);
+            console.log(path.resolve(inputFile), path.resolve(outputFile));
+            console.log('----------');
+            let newInputFile;
+            // 判断文件是否存在
+            fs.access(inputFile, fs.constants.F_OK, (err) => {
+                console.log(`${inputFile} ${err ? '不存在' : '存在'}`);
+                if (err) {
+                    console.log(/^http(s)?:\/\//.test(inputFile));
+                    if (/^http(s)?:\/\//.test(inputFile)) {
+                        newInputFile = inputFile;
+                    } else {
+                        newInputFile = `http:\/\/${inputFile}`;
+                    }
+                } else {
+                    const sysType = os.type();
+                    switch (sysType) {
+                        case 'Linux':
+                            newInputFile =  `file:\/\/${path.resolve(inputFile)}`;
+                            break;
+                    
+                        default:
+                            newInputFile =  path.resolve(inputFile);
+                            break;
+                    }
+                    
+                }
+            });
            (async () => {
-                const browser = await puppeteer.launch();
+                const browser = await puppeteer.launch({args:['--no-sandbox']});
                 const page = await browser.newPage();
-                await page.goto(path.resolve(inputFile));
+                await page.goto(newInputFile);
                 await page.setViewport({
                     width: Number(shotW) || 790,
                     height: Number(shotH) || (fullPage === 'false' ? 600 : 1),

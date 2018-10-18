de17cfb432f695f8d0a4eadb37037f6308de58ce
neo <zhujie1993@hotmail.com>
2018-10-18 18:18:58 +0800

    部分写法调整

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const fs = require('fs');
 const appInfo = require("./package.json");
 
 program.allowUnknownOption()
-       .version(appInfo.version, '-v, --version')
-       .usage('[options] <input file> <output file>')
-       .option('--shot-w <int>', '设置图片宽度')
-       .option('--shot-h <int>', '设置图片高度')
-       .option('--full-page <bool>', '设置是否截取完整页面, 默认为true')
-       .action(function(inputFile, outputFile) {
+    .name('wptoimage')
+    .version(appInfo.version, '-v, --version')
+    .usage('[options] <input file> <output file>')
+    .option('-x, --shot-w <int>', '设置图片宽度')
+    .option('-y, --shot-h <int>', '设置图片高度')
+    .option('--no-full-page', '取消截取完整页面, 默认宽为860， 高为600')
+    .action(function (inputFile, outputFile) {
+        try {
             const { shotW, shotH, fullPage } = program;
             let newInputFile;
             // 判断文件是否存在
@@ -28,30 +30,36 @@ program.allowUnknownOption()
                     const sysType = os.type();
                     switch (sysType) {
                         case 'Linux':
-                            newInputFile =  `file:\/\/${path.resolve(inputFile)}`;
+                            newInputFile = `file:\/\/${path.resolve(inputFile)}`;
                             break;
-                    
+
                         default:
-                            newInputFile =  path.resolve(inputFile);
+                            newInputFile = path.resolve(inputFile);
                             break;
                     }
-                    
+
                 }
             });
-           (async () => {
-                const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+            (async () => {
+                const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
                 const page = await browser.newPage();
                 await page.goto(newInputFile, { timeout: 60000 });
                 await page.setViewport({
-                    width: Number(shotW) || 790,
-                    height: Number(shotH) || (fullPage === 'false' ? 600 : 1),
+                    width: Number(shotW) || (fullPage ? 1 : 860),
+                    height: Number(shotH) || (fullPage ? 1 : 600),
                 });
                 await page.screenshot({
-                path: path.resolve(outputFile),
+                    path: path.resolve(outputFile),
                     type: 'jpeg',
-                    fullPage: fullPage === 'false' ? false : true,
+                    // quality: 100, // 默认质量普通页面和质量100普通页面肉眼查看没有区别，但是大小基本翻倍
+                    fullPage,
                 });
                 await browser.close();
             })();
-       })
-       .parse(process.argv);
+        }
+        catch (err) {
+            program.outputHelp();
+            console.error(err);
+        }
+    })
+    .parse(process.argv);
